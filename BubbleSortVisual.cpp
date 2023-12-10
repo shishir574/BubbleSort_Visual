1ba68b5303e034c147d1b33d3625e498fc0fd43d
shishir574 <shishirgovinda11@gmail.com>
2023-12-10 09:51:07 +0530

    commit from git bash

--- BubbleSortVisual.cpp
+++ BubbleSortVisual.cpp
@@ -18,27 +18,12 @@ void draw_state(vector<int>& v, SDL_Renderer* renderer,unsigned int red_i, unsig
             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderDrawLine(renderer, index, 99, index, i);
         index++;
+
     }
 }
 
-int main(int argc, char* argv[])
+void bubbleSort(vector<int>& v,SDL_Renderer* renderer)
 {
-    random_device rd;
-    uniform_int_distribution d(1, 99);
-    vector<int> v;
-
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    SDL_CreateWindowAndRenderer(100 * 10, 100 * 10, 0, &window, &renderer);
-    SDL_RenderSetScale(renderer, 10, 10);
-
-    //populating vector v
-    for (int i = 0; i < 100; i++)
-    {
-        v.push_back(d(rd));
-    }
-    
-    //bubble sort
     for (unsigned int i = 0; i < v.size(); i++)
     {
         for (unsigned int j = 0; j < v.size(); j++)
@@ -58,6 +43,50 @@ int main(int argc, char* argv[])
             SDL_Delay(2);
         }
     }
+}
+
+int main(int argc, char* argv[])
+{
+    random_device rd;
+    uniform_int_distribution d(1, 99);
+    vector<int> v;
+    char choice;
+    int user_len;
+    int user_val;
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_CreateWindowAndRenderer(100 * 10, 100 * 10, 0, &window, &renderer);
+    SDL_RenderSetScale(renderer, 10, 10);
+
+    cout << "\n Bubble Sort Menu\n";
+    cout << "\n Do you want to enter the values: (y/n)";
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 'n': 
+            for (int i = 0; i < 100; i++)
+            {
+                v.push_back(d(rd));
+            }
+            break;
+        case 'y':
+            cout << "\n How many values do you want to enter (max 100):  ";
+            cin >> user_len;
+            cout << "\n Enter " << user_len << " values between 1 to 99: ";
+            for (int i = 0; i < user_len; i++)
+            {
+                cin >> user_val;
+                v.push_back(user_val);
+            }
+            break;
+        default:
+            cout << "Invalid input";
+            break;
+    }
+
+    bubbleSort(v, renderer);
      
     return 0;
 }
